10e310b62da6451b8de6d474fff8f3c25face8bd
Travis Matthews <tmatthew@e1z2r6p14.42.us.org>
2019-10-16 17:14:08 -0700

    print err msg when execve fails, closes #2

--- src/manage.c
+++ src/manage.c
@@ -6,7 +6,7 @@
 /*   By: tmatthew <tmatthew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 23:29:01 by tmatthew          #+#    #+#             */
-/*   Updated: 2019/10/16 17:02:48 by tmatthew         ###   ########.fr       */
+/*   Updated: 2019/10/16 17:11:48 by tmatthew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 int		manage_exec(t_context *ctx, char *envp[])
 {
 	errno = 0;
-	DEBUG_LOG("execve: %s\n", ctx->command[0]);
 	execve(ctx->command[0], ctx->command, envp);
-	ft_printf("ft_script: %s: %s", ctx->command[0], , strerror(errno));
+	ft_printf("ft_script: %s: %s\n", ctx->command[0], strerror(errno));
 	return (-1);
 }
 
